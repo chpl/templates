df1231f6204ae90645f1613d8282108998e8e50b
ChaimP <chaim.platonov@env0.com>
2023-06-18 14:01:21 +0300

    TF module usage

--- module-registry/usage/main.tf
+++ module-registry/usage/main.tf
@@ -1,8 +1,19 @@
-module "gh_null" {
-  source = "api-pr12306.dev.env0.com/737bb6df-1d96-4cec-825b-1e957dc54b95/null/github"
+module "gh_private_null" {
+  source  = "api-pr12306.dev.env0.com/737bb6df-1d96-4cec-825b-1e957dc54b95/gh_private/null"
   version = "1.0.0"
+  name    = "private"
 }
 
-output "name" {
-  value = module.gh_null.message
+output "private_output_name" {
+  value = module.gh_private_null.message
+}
+
+module "gh_public_null" {
+  source  = "api-pr12306.dev.env0.com/737bb6df-1d96-4cec-825b-1e957dc54b95/gh_public/null"
+  version = "1.0.0"
+  name    = "public"
+}
+
+output "public_output_name" {
+  value = module.gh_public_null.message
 }
